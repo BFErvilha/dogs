e9d6c1bf627cb5387511bfefed06c57f3b139b4e
bruno <bruno.ervilha1410@gmail.com>
2020-09-11 20:49:05 -0300

    Login BG - Login Create - UseFetch - Protected Route - UseHeader

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,11 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha deve ter no minimo 1 caracter maíusculo, 1 minúsculo e 1 digito com pelo menos 8 caracteres.',
+  },
 };
 
 const useForm = (type) => {
@@ -16,7 +21,7 @@ const useForm = (type) => {
     if (value.length === 0) {
       setError('Preencha um valor.');
       return false;
-    } else if (types[type] && types[type].regex.test(value)) {
+    } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
       return false;
     } else {
